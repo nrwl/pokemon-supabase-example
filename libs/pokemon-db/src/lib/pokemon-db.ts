eaaf59dfc676ec9453ef8fbfb3ebbd214a6aa6c7
Zack DeRose <zack.derose@gmail.com>
2022-08-06 14:59:09 -0700

    updating sql schema and adding ts types

--- libs/pokemon-db/src/lib/pokemon-db.ts
+++ libs/pokemon-db/src/lib/pokemon-db.ts
@@ -1,3 +1,81 @@
-export function pokemonDb(): string {
-  return 'pokemon-db';
+export interface PokemonTableEntity {
+  id: string;
+  name: string;
+  type1: Type;
+  type2: Type | undefined;
+  classification: string;
+  height: number;
+  weight: number;
+  captureRate: number;
+  imageUrl: string;
+  baseStatHp: number;
+  baseStatAttack: number;
+  baseStatDefense: number;
+  baseStatSpecial: number;
+  baseStatSpeed: number;
+  level50MinHp: number;
+  level50MaxHp: number;
+  level50MinAttack: number;
+  level50MaxAttack: number;
+  level50MinDefense: number;
+  level50MaxDefense: number;
+  level50MinSpecial: number;
+  level50MaxSpecial: number;
+  level50MinSpeed: number;
+  level50MaxSpeed: number;
+  level100MinHp: number;
+  level100MaxHp: number;
+  level100MinAttack: number;
+  level100MaxAttack: number;
+  level100MinDefense: number;
+  level100MaxDefense: number;
+  level100MinSpecial: number;
+  level100MaxSpecial: number;
+  level100MinSpeed: number;
+  level100MaxSpeed: number;
 }
+
+export interface MoveTableEntity {
+  id: string;
+  name: string;
+  type: Type;
+  description: string;
+  attack: number;
+  accuracy: number;
+  powerPoints: number;
+  effectPercentage: number;
+}
+
+export interface TypeTableEntity {
+  name: Type;
+}
+
+export interface MovesLearnedByLevelTableEntity {
+  pokemonId: string;
+  moveId: string;
+  level: number | undefined;
+}
+
+export interface MovesLearnedByItem {
+  pokemonId: string;
+  moveId: string;
+  itemName: string;
+}
+
+export const types = [
+  'water',
+  'fire',
+  'grass',
+  'electric',
+  'psychic',
+  'normal',
+  'fighting',
+  'poison',
+  'ground',
+  'rock',
+  'bug',
+  'ghost',
+  'dragon',
+] as const;
+
+export type Type = typeof types[number];
--- libs/pokemon-db/supabase/migrations/schema.sql
+++ libs/pokemon-db/supabase/migrations/schema.sql
@@ -2,8 +2,45 @@ CREATE TABLE IF NOT EXISTS public.pokemon
 (
     id text NOT NULL,
     name text NOT NULL,
-    type text NOT NULL,
+    type_1 text NOT NULL,
+    type_2 text,
+    classification text NOT NULL,
+    height number NOT NULL,
+    weight number NOT NULL,
+    capture_rate number NOT NULL,
+    image_url text NOT NULL,
+    base_stat_hp number NOT NULL,
+    base_stat_attack number NOT NULL,
+    base_stat_defense number NOT NULL,
+    base_stat_special number NOT NULL,
+    base_stat_speed number NOT NULL,
+    lv_50_min_hp number NOT NULL,
+    lv_50_max_hp number NOT NULL,
+    lv_50_min_attack number NOT NULL,
+    lv_50_max_attack number NOT NULL,
+    lv_50_min_defense number NOT NULL,
+    lv_50_max_defense number NOT NULL,
+    lv_50_min_special number NOT NULL,
+    lv_50_max_special number NOT NULL,
+    lv_50_min_speed number NOT NULL,
+    lv_50_max_speed number NOT NULL,
+    lv_100_min_hp number NOT NULL,
+    lv_100_max_hp number NOT NULL,
+    lv_100_min_attack number NOT NULL,
+    lv_100_max_attack number NOT NULL,
+    lv_100_min_defense number NOT NULL,
+    lv_100_max_defense number NOT NULL,
+    lv_100_min_special number NOT NULL,
+    lv_100_max_special number NOT NULL,
+    lv_100_min_speed number NOT NULL,
+    lv_100_max_speed number NOT NULL,
     CONSTRAINT pokemon_pkey PRIMARY KEY (id)
+    CONSTRAINT pokemon_type_1_fkey FOREIGN KEY (type_1)
+        REFERENCES public.types (name) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION,
+    CONSTRAINT pokemon_type_2_fkey FOREIGN KEY (type_2)
+        REFERENCES public.types (name) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION,
 )
 
 TABLESPACE pg_default;
@@ -14,22 +51,51 @@ CREATE TABLE IF NOT EXISTS public.moves
     name text NOT NULL,
     type text NOT NULL,
     description text NOT NULL,
+    attack number,
+    accuracy number,
+    power_points number,
+    effect_percentage number,
     CONSTRAINT move_pkey PRIMARY KEY (id)
 )
 
 TABLESPACE pg_default;
 
-CREATE TABLE IF NOT EXISTS public.learnable_moves
+CREATE TABLE IF NOT EXISTS public.types
 (
-    pokemonId text NOT NULL,
-    moveId text NOT NULL,
-    CONSTRAINT learnable_moves_pkey PRIMARY KEY (pokemonId, moveId),
-    CONSTRAINT learnable_moves_pokemonId_fkey FOREIGN KEY (pokemonId)
+    name text NOT NULL,
+    CONSTRAINT type_pkey PRIMARY KEY (name)
+)
+
+TABLESPACE pg_default;
+
+
+CREATE TABLE IF NOT EXISTS public.moves_learned_by_level
+(
+    pokemon_Id text NOT NULL,
+    move_id text NOT NULL,
+    level number,
+    CONSTRAINT moved_learned_by_level_pkey PRIMARY KEY (pokemon_id, move_id),
+    CONSTRAINT moves_learned_by_level_pokemon_id_fkey FOREIGN KEY (pokemon_id)
         REFERENCES public.pokemon (id) MATCH SIMPLE
         ON UPDATE NO ACTION ON DELETE NO ACTION,
-    CONSTRAINT learnable_moves_moveId_fkey FOREIGN KEY (moveId)
+    CONSTRAINT moves_learned_by_level_move_id_fkey FOREIGN KEY (move_id)
         REFERENCES public.moves (id) MATCH SIMPLE
         ON UPDATE NO ACTION ON DELETE NO ACTION
 )
+
+TABLESPACE pg_default;
+
+CREATE TABLE IF NOT EXISTS public.moves_learned_by_item {
+    pokemon_id text NOT NULL,
+    move_id text NOT NULL,
+    item_name text NOT NULL,
+    CONSTRAINT moves_learned_by_item_pkey PRIMARY KEY (pokemon_id, move_id),
+    CONSTRAINT moves_learned_by_item_pokemon_id_fkey FOREIGN KEY (pokemon_id)
+        REFERENCES public.pokemon (id) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION,
+    CONSTRAINT moves_learned_by_item_move_id_fkey FOREIGN KEY (move_id)
+        REFERENCES public.moves (id) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION
+}
 
 TABLESPACE pg_default;
